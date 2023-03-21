4037d67d27bc640e1b6ff9cb908a0c9e21f01a14
Guillem <guillemmj33@gmail.com>
2023-03-21 12:21:15 +0100

    navbar and footer ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <footer class=' bg-white shadow dark:bg-gray-800'>
-      <div class='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between'>
-        <span class='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+    <footer className='bg-white shadow dark:bg-white fixed bottom-0 left-0 right-0'>
+      <div className='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between'>
+        <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
           © 2023{' '}
-          <Link href='/' class='hover:underline'>
+          <a href='/' className='hover:underline'>
             Allegro™
-          </Link>
+          </a>
           . All Rights Reserved. Made by Guillem Martínez.
         </span>
-        <ul class='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+        <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
           <li>
-            <Link href='/about' class='mr-4 hover:underline md:mr-6 '>
+            <a href='/about' className='mr-4 hover:underline md:mr-6 '>
               About
-            </Link>
+            </a>
           </li>
           <li>
-            <Link href='/contact' class='hover:underline'>
+            <a href='/contact' className='hover:underline'>
               Contact
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
-      <div class='container flex flex-wrap items-center justify-between mx-auto'>
-        <Link href='/' class='flex items-center'>
+    <nav className='fixed top-0 left-0 right-0 bg-white border-gray-200 px-2 border-b sm:px-4 py-2.5 dark:bg-white'>
+      <div className='container flex flex-wrap items-center justify-between mx-auto'>
+        <a href='/' className='flex items-center'>
+          <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
           <img
-            src='https://flowbite.com/docs/images/logo.svg'
-            class='h-6 mr-3 sm:h-9'
-            alt='Allegro Logo'
+            src='/src/assets/allegro-text.png'
+            className='h-6 mr-3 sm:h-9'
+            alt='Allegro Logo Text'
           />
-          <span class='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
-            Flowbite
           </span>
-        </Link>
+        </a>
         <button
           data-collapse-toggle='navbar-default'
           type='button'
-          class='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
           aria-controls='navbar-default'
           aria-expanded='false'
         >
-          <span class='sr-only'>Open main menu</span>
+          <span className='sr-only'>Open main menu</span>
           <svg
-            class='w-6 h-6'
+            className='w-6 h-6'
             aria-hidden='true'
             fill='currentColor'
             viewBox='0 0 20 20'
@@ -37,12 +35,12 @@ export default function Navbar() {
             ></path>
           </svg>
         </button>
-        <div class='hidden w-full md:block md:w-auto' id='navbar-default'>
-          <ul class='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+        <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
+          <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-white md:dark:bg-white dark:border-gray-700'>
             <li>
               <a
-                href='#'
-                class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                href='/'
+                className='block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-cyan-500 dark:hover:bg-cyan-500 dark:hover:text-cyan-500 md:dark:hover:bg-transparent'
                 aria-current='page'
               >
                 Home
@@ -50,18 +48,34 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href='#'
-                class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                href='/about'
+                className='block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-cyan-500 dark:hover:bg-cyan-500 dark:hover:text-cyan-500 md:dark:hover:bg-transparent'
               >
                 About
               </a>
             </li>
             <li>
               <a
-                href='#'
-                class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                href='/contact'
+                className='block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-cyan-500 dark:hover:bg-cyan-500 dark:hover:text-cyan-500 md:dark:hover:bg-transparent'
               >
                 Contact
+              </a>
+            </li>
+            <li>
+              <a
+                href='/login'
+                className='block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-cyan-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+              >
+                <i className="fa-solid fa-user"></i> Login or register
+              </a>
+            </li>
+            <li>
+              <a
+                href='/post'
+                className='block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-cyan-500 dark:hover:bg-cyan-500 dark:hover:text-cyan-500 md:dark:hover:bg-transparent'
+              >
+                <i className="fa-solid fa-circle-plus"></i> Upload product
               </a>
             </li>
           </ul>
