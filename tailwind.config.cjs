e17760741c67b432fca981308be8825b2353ee7d
Guillem <guillemmj33@gmail.com>
2023-03-24 00:00:39 +0100

    major style fix 4

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
+  plugins: [],
 }
