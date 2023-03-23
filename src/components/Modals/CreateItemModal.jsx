d946fa81776c83f2e74df4691b05062067cfa285
Guillem <guillemmj33@gmail.com>
2023-03-23 15:59:08 +0100

    CORS api added

--- src/components/Modals/CreateItemModal.jsx
+++ src/components/Modals/CreateItemModal.jsx
@@ -116,7 +116,7 @@ export default function CreateItemModal() {
                 </label>
                 <input
                   type='text'
-                  name='location'
+                  name='location' 
                   id='location'
                   className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-slate-900 dark:focus:ring-primary-500 dark:focus:border-primary-500'
                   placeholder='Product location'
